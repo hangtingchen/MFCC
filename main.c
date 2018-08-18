5ea34ab065f4d40277ef0252cb31dae5e0c41f7d
hangtingchen <chenhangting17@mails.ucas.ac.cn>
2018-08-19 00:54:14 +0800

    temp upload

--- main.c
+++ main.c
@@ -12,6 +12,9 @@
 #define maxBuffLength 600
 
 using namespace hmath;
+using namespace sigProcess;
+using namespace hWAVE;
+
 
 /*含义
 pi
@@ -115,18 +118,18 @@ int main(int argc, char** argv) {
 	int regreOrder;
 	int MFCC0thFlag;
 	int fftLength;
-	FILE *f ;FILE* f_result;//分别是原文件,处理后的十进制文件
+	FILE *f; FILE* f_result;//分别是原文件,处理后的十进制文件
 	FILE *fList;
-	char* pcmFile1;char* pcmFile2;char fileNameBuf[maxBuffLength];
-	Vector d2 , d3 ,fbank,subBankEnergy=NULL;
-	Vector* d1;
+	char* pcmFile1; char* pcmFile2; char fileNameBuf[maxBuffLength];
+	Vector d2 = NULL, d3 = NULL, fbank = NULL, subBankEnergy = NULL, hamWin = NULL;
+	Vector* d1 = NULL;
 	FBankInfo info;
 	Vector dpostProc;
 	int otherFeatureNum;
 	int curr_pos = 1;
-	int vSize,rowNum,step;
-	double zc,te,te2,brightness;
-	int i,j,i0,j0;
+	int vSize, rowNum, step;
+	double zc, te, te2, brightness;
+	int i, j, i0, j0;
 
 	i0 = ini_parse(argv[1], handler, &config);
 	if (i0<0) {
@@ -135,7 +138,7 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 	else if (i0 > 0) {
-		printf("Unknown input config settings in line %d",i0);
+		printf("Unknown input config settings in line %d", i0);
 		system("pause");
 		return 1;
 	}
@@ -159,7 +162,7 @@ int main(int argc, char** argv) {
 	regreOrder = config.regreOrder;
 	MFCC0thFlag = config.MFCC0thFlag;
 	fftLength = config.fftLength;
-	fList=fopen(config.fileList,"r");
+	fList = fopen(config.fileList, "r");
 	otherFeatureNum = MFCC0thFlag + energyFlag + zeroCrossingFlag + brightFlag + subBandEFlag + fftLength;//计算其他参量的个数
 
 	subBankEnergy = CreateVector(subBandEFlag);
@@ -168,26 +171,23 @@ int main(int argc, char** argv) {
 	d3 = CreateVector(MFCCNum);
 
 	info = InitFBank(wlen, samplePeriod, bankNum, lowpassfre, hipassfre, 1, 1, 0, 1.0, 0, 0);
-	//初始化，其中wlen，采样间隔=(10E7/16000),bankNum,最低频率,最高频率,是否计算能量，
-	//是否加log，是否doublefft，是否resacle(1.0不进行)
+	//初始化，其中wlen，采样间隔=(10E7/16000),bankNum,最低频率,最高频率,是否计算能量，是否加log，是否doublefft，是否resacle(1.0不进行)
 	//产生ham窗
-	GenHamWindow(wlen);
+	hamWin = GenHamWindow(wlen);
 
-	while(fgets(fileNameBuf,maxBuffLength,fList)!=NULL){
+	while (fgets(fileNameBuf, maxBuffLength, fList) != NULL) {
 		curr_pos = 1;
-		if(fileNameBuf[0]=='\n')break;
-		pcmFile1=strtok(fileNameBuf,"\t");
-		pcmFile2=strtok(NULL,"\n");
+		if (fileNameBuf[0] == '\n')break;
+		pcmFile1 = strtok(fileNameBuf, "\t");
+		pcmFile2 = strtok(NULL, "\n");
 		/*1.读取wav文件, 并转化为十进制*/
 		//打开原文件
 		printf("Convert %s to %s\n", pcmFile1, pcmFile2);
 		f = fopen(pcmFile1, "rb");
 		if (!f) { printf("open .wav failed\n"); system("pause");  return -1; }
 
-		printf("including : \nMFCCNum	%d\nenergyFlag %d\nzeroCrossingFlag %d\nbrightFlag %d\nsubBandEFlag %d\n",
-			MFCCNum, energyFlag, zeroCrossingFlag, brightFlag, subBandEFlag);
-		printf("the frame feature dimension is %d\n",(MFCCNum + otherFeatureNum) 
-			* config.vecNum* regreOrder);
+		printf("including : \nMFCCNum	%d\nenergyFlag %d\nzeroCrossingFlag %d\nbrightFlag %d\nsubBandEFlag %d\n", MFCCNum, energyFlag, zeroCrossingFlag, brightFlag, subBandEFlag);
+		printf("the frame feature dimension is %d\n", (MFCCNum + otherFeatureNum) * config.vecNum* regreOrder);
 		printf("order %d\n", regreOrder);
 		printf("start...\n");
 
@@ -199,10 +199,8 @@ int main(int argc, char** argv) {
 			system("pause");
 			return 1;
 		}
-		config.sampleNum=wavfile.WAVEParams.numSamples; 
-		config.channels = wavfile.WAVEParams.numChannels;
-		if(config.channels==2)d1 = CreateMatrix(wavfile.WAVEParams.numChannels*2, 
-			wavfile.WAVEParams.numSamples);
+		config.sampleNum = wavfile.WAVEParams.numSamples; config.channels = wavfile.WAVEParams.numChannels;
+		if (config.channels == 2)d1 = CreateMatrix(wavfile.WAVEParams.numChannels * 2, wavfile.WAVEParams.numSamples);
 		else d1 = CreateMatrix(wavfile.WAVEParams.numChannels, wavfile.WAVEParams.numSamples);
 		for (i0 = 1; i0 <= wavfile.WAVEParams.numChannels; i0++)
 			for (i = 1; i <= wavfile.WAVEParams.numSamples; i++) {
@@ -212,11 +210,10 @@ int main(int argc, char** argv) {
 			d1[3][i] = 0.5*(d1[1][i] + d1[2][i]);
 			d1[4][i] = d1[1][i] - d1[2][i];
 		}
-		for(i0 = 1; i0 <= NumRows(d1); i0++) PreEmphasise(d1[i0],config.preemphasise);
+		for (i0 = 1; i0 <= NumRows(d1); i0++) PreEmphasise(d1[i0], config.preemphasise);
 		fclose(f); free_WAVE(&wavfile);
 
-		dpostProc = CreateVector(((MFCCNum + otherFeatureNum) * config.vecNum* 
-			regreOrder *(int)((config.sampleNum - (wlen - inc)) / inc)));
+		dpostProc = CreateVector(((MFCCNum + otherFeatureNum) * config.vecNum* regreOrder *(int)((config.sampleNum - (wlen - inc)) / inc)));
 		printf("total coef size: %d\n", VectorSize(dpostProc));
 
 		/*2.计算MFCC系数以及其他参量*/
@@ -224,26 +221,24 @@ int main(int argc, char** argv) {
 		//最终的数据
 		for (j = 0; j <= (config.sampleNum - wlen); j += inc) {
 			if (config.vecNum > NumRows(d1)) {
-				printf("vecnum %d > num_channels %d ",config.vecNum,config.channels);
+				printf("vecnum %d > num_channels %d ", config.vecNum, config.channels);
 				system("pause");
 				return 1;
 			}
 			for (i0 = 1; i0 <= config.vecNum; i0++) {
-				if (config.vecNum == 1 && config.channels == 2)i0 = 3;
 				for (i = 1; i <= wlen; i++)d2[i] = d1[i0][i + j];
-//				if (j == 0)ShowVector(d2);
+				//				if (j == 0)ShowVector(d2);
 				//计算过零率
 				zc = zeroCrossingRate(d2, wlen);
 				//加窗
-				Ham(d2);
+				Ham(d2, hamWin, wlen);
 				//te和te2分别是根据信号和fft后的信号计算的能量
 				//经过mel滤波器组
 				Wave2FBank(d2, fbank, &te, &te2, info);
-//				if (j == 0)ShowVector(fbank);
+				//				if (j == 0)ShowVector(fbank);
 				//计算谱中心和子带能量，都是百分数
-				brightness = 0.0; 
-				calBrightness(info.x, &brightness, te2); 
-				if (subBandEFlag)calSubBankE(info.x, subBankEnergy, te2);
+				brightness = calBrightness(info.x);
+				if (subBandEFlag)calSubBankE(info.x, subBankEnergy);
 				//计算MFCC系数
 				if (config.fbankFlag) CopyVector(fbank, d3);
 				else FBank2MFCC(fbank, d3, MFCCNum);;
@@ -253,59 +248,48 @@ int main(int argc, char** argv) {
 				if (energyFlag) { dpostProc[curr_pos] = log(te); curr_pos++; }
 				if (zeroCrossingFlag) { dpostProc[curr_pos] = zc; curr_pos++; } //printf("%f\n", zc);
 				if (brightFlag) { dpostProc[curr_pos] = brightness; curr_pos++; };
-				if (subBandEFlag) { for (j0 = 1; j0 <= subBandEFlag; j0++, curr_pos++)
-					dpostProc[curr_pos] = subBankEnergy[j0]; }
-				if (fftLength) { for (j0 = 1; j0 <= fftLength; j0++, curr_pos++)
-					dpostProc[curr_pos] = sqrt(info.x[2 * j0 - 1] * info.x[2 * j0 - 1] + info.x[2 * j0] * info.x[2 * j0]); }
+				if (subBandEFlag) { for (j0 = 1; j0 <= subBandEFlag; j0++, curr_pos++)dpostProc[curr_pos] = subBankEnergy[j0]; }
+				if (fftLength) { for (j0 = 1; j0 <= fftLength; j0++, curr_pos++)dpostProc[curr_pos] = sqrt(info.x[2 * j0 - 1] * info.x[2 * j0 - 1] + info.x[2 * j0] * info.x[2 * j0]); }
 			}
-			curr_pos += (MFCCNum + otherFeatureNum) * (regreOrder - 1)* config.vecNum ;
+			curr_pos += (MFCCNum + otherFeatureNum) * (regreOrder - 1)* config.vecNum;
 		}
-		
+
 		printf("post-processing...\n");
 		//计算加速参量
 		vSize = (MFCCNum + otherFeatureNum)*config.vecNum; rowNum = (config.sampleNum - (wlen - inc)) / inc; step = (MFCCNum + otherFeatureNum) * regreOrder*config.vecNum;
-	//	NormaliseLogEnergy(&dpostProc[1 + MFCCNum], rowNum, step, 50.0, 0.1);
+		//	NormaliseLogEnergy(&dpostProc[1 + MFCCNum], rowNum, step, 50.0, 0.1);
 		/*3.能量归一化*/
-	//	NormaliseLogEnergy2(&dpostProc[1 + MFCCNum], rowNum, step);
+		//	NormaliseLogEnergy2(&dpostProc[1 + MFCCNum], rowNum, step);
 		/*4.计算加速系数*/
-		if(regreOrder>=2)Regress(&dpostProc[1], vSize , rowNum, step, vSize, delwin, 0, 0,0);
-		if(regreOrder>=3)Regress(&dpostProc[1+vSize], vSize, rowNum, step, vSize, delwin, 0, 0, 0);
+		if (regreOrder >= 2)Regress(&dpostProc[1], vSize, rowNum, step, vSize, delwin, 0, 0, 0);
+		if (regreOrder >= 3)Regress(&dpostProc[1 + vSize], vSize, rowNum, step, vSize, delwin, 0, 0, 0);
 		/*5.MFCC参量归一归整*/
 		//MFCC系数均值方差归整，不对其他参数做此操作
-	//	FZeroMean(&dpostProc[1], MFCCNum, rowNum, step); FNormalize(&dpostProc[1], MFCCNum, rowNum, step);
-	//	FZeroMean(&dpostProc[1], step, rowNum, step);FNormalize(&dpostProc[1], step, rowNum, step);
-	//	if (regreOrder >= 2){ FZeroMean(&dpostProc[1+vSize], MFCCNum, rowNum, step); FNormalize(&dpostProc[1+vSize], MFCCNum, rowNum, step); }
-	//	if (regreOrder >= 3) { FZeroMean(&dpostProc[1+2*vSize], MFCCNum, rowNum, step); FNormalize(&dpostProc[1+2*vSize], MFCCNum, rowNum, step); }
-		
-	//	扩大
-	//	for (int i = 1; i <= VectorSize(dpostProc); i++)dpostProc[i] *= 10.0;
+		//	FZeroMean(&dpostProc[1], MFCCNum, rowNum, step); FNormalize(&dpostProc[1], MFCCNum, rowNum, step);
+		//	FZeroMean(&dpostProc[1], step, rowNum, step);FNormalize(&dpostProc[1], step, rowNum, step);
+		//	if (regreOrder >= 2){ FZeroMean(&dpostProc[1+vSize], MFCCNum, rowNum, step); FNormalize(&dpostProc[1+vSize], MFCCNum, rowNum, step); }
+		//	if (regreOrder >= 3) { FZeroMean(&dpostProc[1+2*vSize], MFCCNum, rowNum, step); FNormalize(&dpostProc[1+2*vSize], MFCCNum, rowNum, step); }
+
+		//	扩大
+		//	for (int i = 1; i <= VectorSize(dpostProc); i++)dpostProc[i] *= 10.0;
 
 		/*6.写入目标文件*/
-
-		if(config.saveType==0){
-			f_result = fopen(pcmFile2, "w");
-			if (!f_result) { printf("open result.dat failed\n"); system("pause");  return -1; }
-			printf("writing the doc...\n");
-			for(i=1;i<=VectorSize(dpostProc);i++){
+		f_result = fopen(pcmFile2, "w");
+		if (!f_result) { printf("open result.dat failed\n"); system("pause");  return -1; }
+		printf("writing the doc...\n");
+		if (config.saveType == 0) {
+			for (i = 1; i <= VectorSize(dpostProc); i++) {
 				fprintf(f_result, "%f\t", dpostProc[i]);
 				if (i % step == 0)fprintf(f_result, "\n");
 			}
-			fclose(f_result);
 		}
-		else if(config.saveType==1){
-			f_result = fopen(pcmFile2, "w");
-			if (!f_result) { printf("open result.dat failed\n"); system("pause");  return -1; }
-			printf("writing the doc...\n");
-			for(i=1;i<=VectorSize(dpostProc);i++){
+		else if (config.saveType == 1) {
+			for (i = 1; i <= VectorSize(dpostProc); i++) {
 				fprintf(f_result, "%e\t", dpostProc[i]);
 				if (i % step == 0)fprintf(f_result, "\n");
 			}
-			fclose(f_result);
 		}
-		else {
-			cnpy::npy_save(pcmFile2, &dpostProc[1], { (size_t)rowNum ,(size_t)step },"w");
-		}
-
+		fclose(f_result);
 		FreeVector(dpostProc);
 		FreeMatrix(d1);
 	}
@@ -314,6 +298,7 @@ int main(int argc, char** argv) {
 	FreeVector(fbank);
 	FreeVector(d3);
 	FreeVector(subBankEnergy);
+	FreeSVector(hamWin);
 	fclose(fList);
 	//system("pause");
 	return 0;
